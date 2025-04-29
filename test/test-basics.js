5bb48ed01db5b02a2bf70e63b9762ec01e5ed001
Seth Trowbridge <seth111@gmail.com>
2025-04-28 22:57:06 -0400

    more slimming

--- test/test-basics.js
+++ test/test-basics.js
@@ -1,67 +1,39 @@
 const ROOT = /** @type {HTMLDivElement} */ (document.querySelector("[data-boxcast]"));
 const BUTTONS = document.createElement("div");
 document.body.append(BUTTONS);
-/** @type {TextTrackList|undefined} */
-let TRACKS = undefined;
+let TRACKS = /** @type {TextTrack[]} */([]);
 
-const LANGS = [];
-
-const handlerTrack =()=>
-{
-    if(TRACKS)
-    {
-        for(let i=0; i<TRACKS.length; i++)
-        {
-            BUTTONS.children[i].setAttribute("data-active", TRACKS[i].mode == "showing");
-        }
-    }
-};
+const handlerTrack =()=> TRACKS.forEach( (t, i)=>BUTTONS.children[i].setAttribute("data-active", TRACKS[i].mode ));
 
 const scan =()=>
 {
     const video = ROOT.querySelector('video');
-    TRACKS = video?.textTracks;
-    BUTTONS.innerHTML = "";
-    LANGS = [];
-    if(TRACKS)
-    {
-        for(let i=0; i<TRACKS.length; i++)
-        {
-            LANGS[i] = TRACKS[i].label;
-            const button = document.createElement("button");
-            button.innerText = TRACKS[i].label;
-            button.addEventListener("click", ()=>pick(i));
-            BUTTONS.append(button);
-        }
+    TRACKS = Array.from(video?.textTracks);
 
-        TRACKS.removeEventListener("change", handlerTrack);
-        TRACKS.addEventListener("change", handlerTrack);
+    BUTTONS.innerHTML = "";
+    TRACKS.forEach((t, i)=>
+    {
+        const button = document.createElement("button");
+        button.innerText = TRACKS[i].label;
+        button.addEventListener("click", ()=>pick(i));
+        BUTTONS.append(button);        
+    })
+
+    if(video?.textTracks)
+    {
+        ROOT.querySelector("[data-cc-controls]")?.remove();
+        video.textTracks.removeEventListener("change", handlerTrack);
+        video.textTracks.addEventListener("change", handlerTrack);
         handlerTrack();
     }
+
 };
 
 /** @type {(index:number)=>void} */
 const pick =(index)=>
 {
-    if(TRACKS)
-    {
-        if(TRACKS[index].mode == "showing")
-        {
-            index = -1;
-        }
-
-        for(let i=0; i<TRACKS.length; i++)
-        {
-            TRACKS[i].mode = "disabled";
-        }
-    
-        const cc = index !== -1;
-        cc && (TRACKS[index].mode = "showing");
-
-        ROOT.querySelectorAll('i.boxcast-cc-checked').forEach(c=>c.classList.remove("boxcast-icon-check"));
-        ROOT.querySelector(`[data-cc-${index === -1 ? 'disable-captions' : `enable-caption-id-${index}`}-checked]`)?.classList.add('boxcast-icon-check');
-        ROOT.querySelector("button[data-cc-button]")?.classList[cc ? "add" : "remove"]("enabled");
-    }
+    index = TRACKS[index].mode == "showing" ? -1 : index;
+    TRACKS.forEach( (t, i)=>TRACKS[i].mode = (i==index) ? "showing" : "disabled" );
 }
 
 globalThis.scan = scan;
